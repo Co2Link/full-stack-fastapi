6cce068a6e5a17690e09c7d1d92d9adb5dcde28a
lingxt <lxt9601@gmail.com>
2022-11-03 22:12:08 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # full-stack-fastapi
+
+Backend service using FastAPI
+
+## TODO
+
+- CI supprot using Github Action
+- Containerize
+
+## Install python environment with poetry
+
+```
+poetry install
+```
+
+## Run test
+
+```
+pytest
+```
---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.api.api_v1.api import api_router
+from app.core.config import settings
+
+app = FastAPI()
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+@app.get("/")
+def index():
+    return "Hello World"
